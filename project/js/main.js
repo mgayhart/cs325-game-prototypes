e1f2fa18581640d2dbf1f3eaab6a570d256f3157
Matt <mgayhart12@gmail.com>
2018-02-08 14:24:02 -0500

    Text Edit

--- project/js/main.js
+++ project/js/main.js
@@ -38,7 +38,7 @@ window.onload = function() {
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        var text = game.add.text( game.world.centerX, 15, "Take the ship for a ride", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
     
