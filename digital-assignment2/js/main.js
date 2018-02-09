826137ec66a857d38c3a2d90fbcaa029c9e7199a
Matt <mgayhart12@gmail.com>
2018-02-09 13:57:51 -0500

    Fixed main

--- digital-assignment2/js/main.js
+++ digital-assignment2/js/main.js
@@ -13,37 +13,36 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+
     }
     
-    var bouncy;
+    var count;
+    var text;
     
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+        count = 0;
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        text = game.add.text(game.world.centerX, game.world.centerY, "You have 0 dollars!", {
+        	font: "65px Arial",
+        	fill: "#ff0044",
+        	align: "center"
+        });
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        text.anchor.setTo(0.5, 0.5);
+        
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+
+		game.input.onDown.addOnce(updateText, this);
+			
+    }
+    
+    function updateText() {
+    	
+    	count++;
+    	
+    	text.setText("You have " + count + " dollars!");
+    
     }
 };
