d775141dcb2cb0a4a28e46d1b95ce18b3957f914
Matt <mgayhart12@gmail.com>
2018-02-09 13:38:41 -0500

    Begin work on Project 2

--- template-minimal-gamestate/js/main.js
+++ template-minimal-gamestate/js/main.js
@@ -3,38 +3,39 @@
 function make_main_game_state( game )
 {
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+       
     }
     
-    var bouncy;
+    
+    var text;
+    var count;
     
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+       count = 0;
+       
+       text = game.add.text(gam.world.centerX, game.world.centerY, "You have 0 dollars!" {
+       		font: "48px Arial",
+       		fill: "#ff0044",
+       		align: "center"
+       	});
+       	
+       	text.anchor.setTo(0.5, 0.5);
+       		
+       	
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+    
+    	game.input.onDown.addOnce(updateText, this);
+
+    }
+    
+    function updateText(){
+    	
+    	count++;
+    	
+    	text.setText("You have " + count + " dollars!");
+    	
     }
     
     return { "preload": preload, "create": create, "update": update };
