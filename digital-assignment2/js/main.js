8b89926454681e383f0fa366f267a6badc2c6a3e
Matt <mgayhart12@gmail.com>
2018-02-15 14:37:30 -0500

    Finished JS code

--- digital-assignment2/js/main.js
+++ digital-assignment2/js/main.js
@@ -3,10 +3,14 @@
 
 window.onload = function() {
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1200, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-    	game.load.image('circle', 'assets/circle.png');
+    	game.load.image('pollo', 'assets/pollo.png');
+    	game.load.image('chick', 'assets/chicken.png');
+    	game.load.image('egg', 'assets/egg.png');
+    	game.load.image('farm', 'assets/farm.png');
+    	game.load.image('fineDine', 'assets/fineDine.png');
 
     }
     //Text variables used for printing information about each business
@@ -39,6 +43,20 @@ window.onload = function() {
     var driveTime;
     var fineTime;
     
+    //Variables for the amount of money each business produce
+    var eggMoney;
+    var chickenMoney;
+    var farmMoney;
+    var driveMoney;
+    var fineMoney;
+    
+    //Multipliers for businesses that have over 100 owned
+    var eggMult;
+    var chickenMult;
+    var farmMult;
+    var driveMult;
+    var fineMult;
+    
     // Button variables for each business, used to purchase more businesses
     var eggButton;
     var chickenButton;
@@ -56,6 +74,13 @@ window.onload = function() {
         drive = 0;
         fine = 0;
         
+        //set the cost of each business
+        eggCost = 10;
+        chickenCost = 250;
+        farmCost = 1000;
+        driveCost = 10000;
+        fineCost = 50000;
+        
         
         //Set the times in seconds for each of the businesses to produce money
         eggTime = 1;
@@ -64,37 +89,51 @@ window.onload = function() {
         driveTime = 7;
         fineTime = 9;
         
+        //Set Multipliers for the business earnings
+        eggMult = 1;
+        chickenMult = 1;
+        farmMult = 1;
+        driveMult = 1;
+        fineMult = 1;
+       
+       //Set the variables that track the amount of money each business produces 
+        eggMoney = eggs * eggMult * 1;
+        chickenMoney = chicken * chickenMult * 100;
+        farmMoney = farm * farmMult * 500;
+        driveMoney = drive * driveMult * 1000;
+        fineMoney = fine * fineMult * 10000;
+        
         //text creations for the different counters used in the game
-        moneyText = game.add.text(game.world.centerX , game.world.centerY - 250 , "You have 0 Dollars!", {
+        moneyText = game.add.text(1050 , 25 , "You have 0 Dollars!", {
         	font: "25px Arial",
         	fill: "#3ec120",
         	align: "center"
         });
-        eggText = game.add.text(game.world.centerX - 150, game.world.centerY - 200, "You have " + eggs+ " Eggs!", {
+        eggText = game.add.text(110, game.world.centerY - 235, "You have " + eggs + " Eggs! They are producing $" + eggMoney + " every " + eggTime + " seconds.  1 Egg costs: " + eggCost + " dollars!", {
         	font: "20px Arial",
         	fill: "#ff0044",
         	align: "center"
         });
         
-        chickenText = game.add.text(game.world.centerX - 150, game.world.centerY - 175, "You have " + chicken + " Chickens!", {
+        chickenText = game.add.text(110, game.world.centerY - 115, "You have " + chicken + " Chickens! They are producing $" + chickenMoney + " every " + chickenTime + " seconds.  1 Chicken costs: " + chickenCost + " dollars!" , {
         	font: "20px Arial",
         	fill: "#ff0044",
         	align: "center"
         });
         
-        farmText = game.add.text(game.world.centerX - 150, game.world.centerY - 150, "You have " + farm + " Farms!", {
+        farmText = game.add.text(110, game.world.centerY + 10, "You have " + farm + " Farms! They are producing $" + farmMoney + " every " + farmTime + " seconds.  1 Farm costs: " + farmCost + " dollars!", {
         	font: "20px Arial",
         	fill: "#ff0044",
         	align: "center"
         });
         
-        driveText = game.add.text(game.world.centerX - 150, game.world.centerY - 125, "You have " + drive + " Drive-Thru's!", {
+        driveText = game.add.text(110, game.world.centerY + 135, "You have " + drive + " Drive-Thru's! They are producing $" + driveMoney + " every " + driveTime + " seconds.  1 Drive-Thru costs: " + driveCost + " dollars!", {
         	font: "20px Arial",
         	fill: "#ff0044",
         	align: "center"
         });
         
-        fineText = game.add.text(game.world.centerX - 150, game.world.centerY - 100, "You have " + fine + " Fine Dining Restaurants!", {
+        fineText = game.add.text(110, game.world.centerY + 260, "You have " + fine + " Fine Dining Restaurants! They are producing $" + fineMoney + " every " + fineTime + " seconds.  1 Restaurant costs: " + fineCost + " dollars!", {
         	font: "20px Arial",
         	fill: "#ff0044",
         	align: "center"
@@ -108,11 +147,11 @@ window.onload = function() {
         fineText.anchor.setTo(0, 0.5);
        
     //Button creations for each of the businesses 
-        eggButton = game.add.button(game.world.centerX - 375, 0, 'circle', eggAction, this, 2, 1, 0);
-    	chickenButton = game.add.button(25, 130, 'circle', chickenAction, this, 2, 1, 0);
-    	farmButton = game.add.button(25, 260, 'circle', farmAction, this, 2, 1, 0);
-    	driveButton = game.add.button(25, 390, 'circle', driveAction, this, 2, 1, 0);
-    	fineButton = game.add.button(25, 520, 'circle', fineAction, this, 2, 1, 0);
+        eggButton = game.add.button(0, 0, 'egg', eggAction, this, 2, 1, 0);
+    	chickenButton = game.add.button(0, 130, 'chick', chickenAction, this, 2, 1, 0);
+    	farmButton = game.add.button(0, 260, 'farm', farmAction, this, 2, 1, 0);
+    	driveButton = game.add.button(0, 390, 'pollo', driveAction, this, 2, 1, 0);
+    	fineButton = game.add.button(0, 520, 'fineDine', fineAction, this, 2, 1, 0);
     	
     	//money loops 
         game.time.events.loop(Phaser.Timer.SECOND * eggTime, eggPay, this);
@@ -124,28 +163,28 @@ window.onload = function() {
     }
     
     function update() {
-        updateText();	
+        updateText();
     }
     
 
     function eggPay(){
-        money = money + eggs;
+        money = money + eggMoney;
 	}
 	
 	function chickenPay(){
-		money = money + (chicken * 100);
+		money = money + chickenMoney;
 	}
     
     function farmPay(){
-		money = money + (farm * 500);
+		money = money + farmMoney;
 	}
 	
 	function drivePay(){
-		money = money + (drive * 1000);
+		money = money + driveMoney;
 	}
 	
 	function finePay(){
-		money = money + (fine * 10000);
+		money = money + fineMoney;
 	}
     
     function updateText() {
@@ -154,47 +193,52 @@ window.onload = function() {
     
     
     function eggAction(){
-    	if(money - 10 >= 0){
-    		money = money - 10;
+    	if(money - eggCost >= 0){
+    		money = money - eggCost;
     		eggs++;
+    		eggMoney = eggs * eggMult * 1; 
     	}
     	updateText();
-    	eggText.setText("You have " + eggs + " Eggs!");	
+    	eggText.setText("You have " + eggs + " Eggs! Your eggs are producing " + eggMoney + " every " + eggTime + " seconds.  1 Egg costs: " + eggCost + " dollars!");	
     }
     
     function chickenAction(){
-    	if(money - 250 >= 0){
-    		money = money - 250;
+    	if(money - chickenCost >= 0){
+    		money = money - chickenCost;
     		chicken++;
+    		chickenMoney = chicken * chickenMult * 100;
     	}
     	updateText();
-    	chickenText.setText("You have " + chicken + " Chickens!");
+    	chickenText.setText("You have " + chicken + " Chickens! They are producing $" + chickenMoney + " every " + chickenTime + " seconds.  1 Chicken costs: " + chickenCost + " dollars!");
     }
     
     function farmAction(){
-    	if(money - 1000 >= 0){
-    		money = money - 1000;
+    	if(money - farmCost >= 0){
+    		money = money - farmCost;
     		farm++;
+    		farmMoney = farm * farmMult * 500;
     	}
     	updateText();
-    	farmText.setText("You have " + farm + " Farms!");
+    	farmText.setText("You have " + farm + " Farms! They are producing $" + farmMoney + " every " + farmTime + " seconds.  1 Farm costs: " + farmCost + " dollars!");
     }
     
     function driveAction(){
-    	if(money - 10000 >= 0){
-    		money = money - 10000;
+    	if(money - driveCost >= 0){
+    		money = money - driveCost;
     		drive++;
+    		driveMoney = drive * driveMult * 1000;
     	}
     	updateText();
-    	driveText.setText("You have " + drive + " Drive-Thru's!");
+    	driveText.setText("You have " + drive + " Drive-Thru's! They are producing $" + driveMoney + " every " + driveTime + " seconds.  1 Drive-Thru costs: " + driveCost + " dollars!");
     }
     
     function fineAction(){
-    	if(money - 50000 >= 0){
-    		money = money - 50000;
+    	if(money - fineCost >= 0){
+    		money = money - fineCost;
     		fine++;
+    		fineMoney = fine * fineMult * 10000;
     	}
     	updateText();
-    	fineText.setText("You have " + fine + " Fine Dining Restaurants!");
+    	fineText.setText("You have " + fine + " Fine Dining Restaurants! They are producing $" + fineMoney + " every " + fineTime + " seconds.  1 Restaurant costs: " + fineCost + " dollars!");
     }
 };
