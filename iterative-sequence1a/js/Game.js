69b9e6f6283af69f254a3f4260532a01f9d411d3
Matt <mgayhart12@gmail.com>
2018-04-12 14:15:53 -0400

    Iteration begin

---
+++ iterative-sequence1a/js/Game.js
@@ -0,0 +1,152 @@
+"use strict";
+
+GameStates.makeGame = function( game, shared ) {
+    var background;
+    var riddles = [
+    	"What has roots as nobody sees, Is taller than trees, Up, up it goes, And yet never grows?",
+    	"Thirty white horses on a red hill, first they champ, then they stamp, then they stand still.",
+    	"Voiceless it cries, Wingless flutters, Toothless bites, Mouthless mutters.",
+    	"A box without hinges, key, or lid, Yet golden treasure inside is hid.",
+    	"This thing all things devours: Birds, beasts, trees, flowers; Gnaws iron, bites steel; Grinds hard stones to meal; Slays king, ruins town, And beats high mountain down."
+    	];
+    var riddleNum = 0;
+    var solutions = ["mountain", "teeth", "wind", "egg", "time"];
+    var out = "";
+    
+    var text;
+    var wordIndex = 0;
+    var riddleIndex = 0;
+    var wordDelay = 120;
+    var line = [];
+    
+    var bmd;
+    var bmdInput;
+    var bmdComplete;
+    var bmdFail;
+    var enterKey;
+    
+    var strike = 0;
+   	var stateText;
+   	
+    
+    function quitGame() {
+
+        //  Here you should destroy anything you no longer need.
+        //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+		text.destroy();
+		bmd.destroy();
+        //  Then let's go back to the main menu.
+        game.state.start('MainMenu');
+
+    }
+    
+    return {
+    
+        create: function () {
+    		//Add background, NEED TO EDIT THE IMAGE STILL!!
+    		background = game.add.tileSprite(0,0,800,600, 'background');
+    		
+    		text = game.add.text(30,300,'', {font: "35px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 600});
+    		this.nextRiddle();
+    		
+    		//Bitmap Data, for the riddle answers area of text.
+            bmd = game.make.bitmapData(800, 600);
+            bmd.context.font = '64px Arial';
+            bmd.context.fillStyle = '#ffffff';
+            bmd.addToWorld();
+            
+            
+            //Create an enter key, so as to end the input.
+            enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+            enterKey.onDown.add(this.solved, this);
+            //Stop the spacebar from scrolling the browser
+            game.input.keyboard.addKeyCapture(32);
+            
+            //Call Key Press and allow input, so user can answer riddles.
+            game.input.keyboard.addCallbacks(this, null, null, this.keyPress);
+            
+            stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '50px Arial', fill: '#19de65' });
+   	 		stateText.anchor.setTo(0.5, 0.5);
+    		stateText.visible = false;
+           
+           
+        },
+    
+        update: function () {
+    
+          
+        },
+        
+        keyPress: function(char){
+        	bmd.cls();
+        	var x = 64;
+        	out = out + char;
+        	
+        	bmd.context.fillText(out, x, 64);
+        	bmd.addToWorld();
+        },
+        
+        solved(){
+        	if(out === solutions[riddleNum]){
+        		console.log("Decent!");
+        		out = "";
+        		riddleNum++;
+        		text.destroy();
+        		text = game.add.text(30,300,'', {font: "35px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 600});
+        		this.nextRiddle();
+        	}
+        	else{
+        		//Play gollum clip[strike]
+        		strike++;
+        		if(strike === 3){
+        			text.destroy();
+        			stateText.text=" You Lose, try again. \n Click to return to menu";
+        			stateText.visible = true;
+
+        			//the "click to menu" handler
+        			game.input.onTap.addOnce(this.end ,this);
+        			return;
+        		}
+        		out = "";
+        	}
+        },
+        
+        nextRiddle: function(){
+        	if(riddleNum === riddles.length){
+        		//Player won, go to win Scenario
+        		text.destroy();
+        		stateText.text=" You Win! \n Click to return to menu";
+        		stateText.visible = true;
+
+        		//the "click to menu" handler
+        		game.input.onTap.addOnce(this.end,this);
+        		return;
+        	}
+        	else{
+        		if(riddleNum === 0){
+        			//Play mountain clip
+        		}
+        		if(riddleNum === 2){
+        			//play wind clip
+        		}
+        		line = riddles[riddleNum].split(' ');
+        		wordIndex = 0;
+        		game.time.events.repeat(wordDelay, line.length, this.nextWord, this);
+        	}
+        },
+        
+        nextWord: function(){
+        	text.text = text.text.concat(line[wordIndex] + " ");
+        	wordIndex++;
+        	
+        	if(wordIndex === line.length){
+        	
+        	}
+        
+        },
+        
+        end: function(){
+        	quitGame();
+        }
+    };
+};
---
+++ iterative-sequence1a/js/MainMenu.js
@@ -0,0 +1,54 @@
+"use strict";
+
+GameStates.makeMainMenu = function( game, shared ) {
+
+	var music = null;
+	var playButton = null;
+	var lost = null;
+	var text;
+	    
+    function startGame(pointer) {
+
+        //	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+        music.stop();
+
+        //	And start the actual game
+        game.state.start('Game');
+
+    }
+    
+    return {
+    
+        create: function () {
+    
+            //	We've already preloaded our assets, so let's kick right into the Main Menu itself.
+            //	Here all we're doing is playing some music and adding a picture and button
+            //	Naturally I expect you to do something significantly better :)
+    
+            music = game.add.audio('titleMusic');
+            music.play();
+    		lost = game.add.audio('eat');
+            game.add.sprite(0, 0, 'titlePage');
+    
+            playButton = game.add.button( 303, 400, 'playButton', this.speak, null, 'over', 'out', 'down');
+    		
+    		 text = game.add.text(32, 32, '"Welcome to a game of riddles! If you can answer all of the riddles you will escape,\n if you answer incorrectly, but three times you will not.\n Type your answer and press enter, \nIf you get the riddle correct you will see a new riddle, \nMake too many mistakes and your game is over. \nGood luck, click below to begin..."', { font: "15px Arial", fill: "#19de65" });
+
+    		
+        },
+    
+        update: function () {
+    
+            //	Do some nice funky main menu effect here
+    
+        },
+        
+        speak: function(){
+        	music.stop();
+        	lost.play();
+        	lost.onStop.add(startGame, null, this);
+        	
+        }
+        
+    };
+};
