709c8fe00845cb799e8beaa9803ceacc92734a6c
Matt <mgayhart12@gmail.com>
2018-04-26 12:58:37 -0400

    canvas?

--- iterative-sequence2/js/main.js
+++ iterative-sequence2/js/main.js
@@ -10,7 +10,7 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
        game.load.image('Jungle', 'assets/jungleTileset.png');
